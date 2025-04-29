2fa977d81f049028b81e9655d6c25205c497fa25
shinys10 <166797095+sysys10@users.noreply.github.com>
2025-04-30 00:59:47 +0900

    postcard.tsx 업데이트

--- apps/web/src/components/ui/postcard.tsx
+++ apps/web/src/components/ui/postcard.tsx
@@ -18,7 +18,7 @@ export function PostCard({ post }: { post: Post }) {
 
 function PostCardHeader({ nickname, tag }: { nickname: string; tag: string }) {
   return (
-    <div className="flex items-center justify-between w-full">
+    <div className="flex items-center whitespace-nowrap justify-between w-full">
       <div className="flex items-end">
         <span className="text-xl text-gray-500">
           <span className="text-haru-brown font-bold">{nickname}님</span>의 잇루틴
